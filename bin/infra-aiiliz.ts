d4ce7c5dafa1ef4c36419594538e3a1be74f7c31
Ebi Ahmadi <eahmadi641@gmail.com>
2021-05-06 16:34:00 +0300

    feat(app): add default env

--- bin/infra-aiiliz.ts
+++ bin/infra-aiiliz.ts
@@ -7,5 +7,15 @@ import { DataStack } from '../lib/data-stack';
 const ailliz = new cdk.App();
 
 
-new ComputeStack(ailliz, 'ComputeStack');
-new DataStack(ailliz, 'DatabaseStack');
+new ComputeStack(ailliz, 'ComputeStack', {
+    env: { 
+        account: process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEFAULT_REGION,
+      },
+});
+new DataStack(ailliz, 'DatabaseStack', {
+    env: { 
+        account: process.env.CDK_DEFAULT_ACCOUNT, 
+        region: process.env.CDK_DEFAULT_REGION,
+      },
+});
