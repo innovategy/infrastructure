e6fe95489c9ddfe916ba82a4b29001c8e0ad9bdb
Ebi Ahmadi <eahmadi641@gmail.com>
2021-05-07 01:35:34 +0300

    refactor(compute-stack): return certificate to default config

--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -36,7 +36,6 @@ export class ComputeStack extends cdk.Stack {
       domainName: computeConfig.DomainName,
       protocol: computeConfig.Https ?ApplicationProtocol.HTTPS: ApplicationProtocol.HTTP,
       targetProtocol: ApplicationProtocol.HTTP,
-      certificate: ec2.certif,
       redirectHTTP: computeConfig.Https,
       domainZone: zone,
       taskImageOptions: { 
