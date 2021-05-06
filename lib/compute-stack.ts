58abbc2ee11a8455938267b46dd777e66edf4a72
Ebi Ahmadi <eahmadi641@gmail.com>
2021-05-06 03:22:08 +0300

    feat(compute-stack): add hosted zone route53

--- lib/compute-stack.ts
+++ lib/compute-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from '@aws-cdk/core';
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecr from "@aws-cdk/aws-ecr";
+import { PublicHostedZone } from '@aws-cdk/aws-route53';
 import * as ecs_patterns from "@aws-cdk/aws-ecs-patterns";
 import {computeConfig} from "../config/compute.config";
 import { ApplicationProtocol} from '@aws-cdk/aws-elasticloadbalancingv2';
@@ -22,6 +23,10 @@ export class ComputeStack extends cdk.Stack {
       vpc: vpc
     });
 
+    const zone = new PublicHostedZone(this, 'PHZ', {
+      zoneName: computeConfig.DomainName,
+    });
+
     new ecs_patterns.ApplicationLoadBalancedFargateService(this, computeConfig.FargateId, {
       cluster: cluster, 
       cpu: computeConfig.CPUS,
@@ -32,7 +37,7 @@ export class ComputeStack extends cdk.Stack {
       protocol: computeConfig.Https ?ApplicationProtocol.HTTPS: ApplicationProtocol.HTTP,
       targetProtocol: ApplicationProtocol.HTTP,
       redirectHTTP: computeConfig.Https,
-
+      domainZone: zone,
       taskImageOptions: { 
         image: ecs.ContainerImage.fromEcrRepository(repo, "latest") 
       },
