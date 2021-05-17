d75266e25abcc7a5ad80708ad1f86075753399b7
Ebi Ahmadi <eahmadi641@gmail.com>
2021-05-18 00:31:58 +0300

    feat(rds): add rds serverless

--- lib/rds/rds-serverless.ts
+++ lib/rds/rds-serverless.ts
@@ -1,9 +1,12 @@
 import * as rds from '@aws-cdk/aws-rds';
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import { Duration } from '@aws-cdk/core';
+import { Duration, RemovalPolicy } from '@aws-cdk/core';
 
 export default class Serverless {
+  private RDS_ISOLATE_SUBNET_ID:string = "serverlessRdsCluster"
+
+  private isolateSubnetGroupName:string;
 
   private scope: cdk.Construct;
 
@@ -11,17 +14,47 @@ export default class Serverless {
 
   private vpc: ec2.Vpc;
   
+  private backupRetention:number;
+
+  private autoPauseAfter:number;
+
+  private databaseName:string;
+
+
   public build(): rds.ServerlessCluster{
     return new rds.ServerlessCluster(this.scope, this.clusterName, {
         engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
         parameterGroup: rds.ParameterGroup.fromParameterGroupName(this.scope, 'ParameterGroup', 'default.aurora-postgresql10'),
-        subnetGroup: this.vpc.isolatedSubnets,
+        backupRetention: Duration.days(this.backupRetention),
         scaling: {
-            autoPause: Duration.minutes(10), 
+            autoPause: Duration.minutes(this.autoPauseAfter), 
             minCapacity: rds.AuroraCapacityUnit.ACU_8, 
             maxCapacity: rds.AuroraCapacityUnit.ACU_32,
           },
         vpc: this.vpc,
+        vpcSubnets: rds.SubnetGroup.fromSubnetGroupName(this.scope, this.RDS_ISOLATE_SUBNET_ID, this.isolateSubnetGroupName),
+        defaultDatabaseName: this.databaseName,
+        removalPolicy: RemovalPolicy.RETAIN,
       });
+  }
+
+  public backupDurationInDays(days:number=1):Serverless{
+    this.backupRetention = days;
+    return this;
+  }
+
+  public autoPauseClusterAfter(mins:number):Serverless{
+    this.autoPauseAfter = mins;
+    return this;
+  }
+
+  public inIsolateSubnetGroup(name:string):Serverless{
+    this.isolateSubnetGroupName = name;
+    return this;
+  }
+
+  public withDatabaseName(name:string):Serverless{
+    this.isolateSubnetGroupName = name;
+    return this;
   }
 }
--- stacks/database-stack.ts
+++ stacks/database-stack.ts
@@ -1,24 +1,17 @@
 import * as cdk from '@aws-cdk/core';
-import * as rds from '@aws-cdk/aws-rds';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import { computeConfig } from '../../config/compute-cluster.config';
-import { databaseConfig } from '../../config/database-cluster.config';
+import Serverless from '../lib/rds/rds-serverless';
+import Config from '../config/rds.config';
+import VPC from '../config/vpc.config';
 
 export class DataStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = ec2.Vpc.fromLookup(this, 'vpc', {
-      isDefault: false,
-      vpcName: 'ComputeStack/' + computeConfig.VpcName,
-    });
-
-    const cluster = new rds.ServerlessCluster(this, databaseConfig.Identifier, {
-      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
-      parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql10'),
-      defaultDatabaseName: databaseConfig.Name,
-      deletionProtection: databaseConfig.Deleteable,
-      vpc,
-    });
+    new Serverless()
+        .autoPauseClusterAfter(Config.getAutoPauseDuration())
+        .backupDurationInDays(Config.getBackupRetentionDurationPolicy())
+        .inIsolateSubnetGroup(VPC.getIsolateSubnetName())
+        .withDatabaseName(Config.getDatabaseName())
+        .build();
   }
 }
